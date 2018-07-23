40896dc3ba0e13aff0593481dadc0d1e9bc5ee83
Glitch (wonderful-rail) <none>
2018-07-23 19:30:07 +0000

    add saudações a função diga_ola

--- src/index.js
+++ src/index.js
@@ -12,15 +12,42 @@ const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: true })
 
 console.log('bot server running...');
 
+function getRandom(arr){
+  return arr[Math.floor(Math.random()* arr.length)];
+ }
+
 bot.onText(/\/start/, (msg) => {
     bot.sendMessage(msg.chat.id, 'Beep beep boop\n\nOlá, eu sou o bot do grupo FEMUG-PE');
     bot.sendMessage(msg.chat.id, 'O Front-End Meet-Up Group é um projeto que nasceu da necessidade de unir os desenvolvedores para que o acesso à informação seja feito em qualquer lugar que haja interesse do aprendizado.');
     bot.sendMessage(msg.chat.id, 'Faça parte do nosso grupo!\nConvite: https://bit.ly/2uOzSxs \nMeetup: https://bit.ly/2LtCpYx \nFacebook: https://bit.ly/2zTiQEp');
 });
 
+  //lista de saudações
 bot.onText(/\/diga_ola/, (msg) => {
-    bot.sendMessage(msg.chat.id, `Beep boop, ${msg.from.first_name}`);
+  const saudacao = [
+    `Beep boop, ${msg.from.first_name}`,
+    `Faaaaaaala ${msg.from.first_name}!`,
+    `Dale ${msg.from.first_name}!`,
+    `Alo alo alo  ${msg.from.first_name}`,
+    `Eae ${msg.from.first_name}`,
+    'É quente..',
+    'É rocha',
+    'Tudo tranquilo ctg?',
+    'Só na marosidade, ${msg.from.first_name}?'
+  ];
+    bot.sendMessage(msg.chat.id, getRandom(saudacao));
 });
+
+//lista de jokes
+bot.onText(/\/joke/, (msg) => {
+  const joke = [
+    'dispense',
+    'é tu nada, visse',
+    'apois viu',
+    'e arroi'
+  ];
+  bot.sendMessage(msg.chat.id, getRandom(joke));
+})
 
 bot.on('new_chat_members', (msg) => {
   bot.sendMessage(msg.chat.id, `Mais um Front-ender chegando no grupo!\n\nSeja bem-vindo(a) ${msg.from.first_name}! Tudo tranquilo? Só na marosidade?`);
